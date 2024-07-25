11cade6f66e64709c27ee88a69888ce55252b013
jerryyang108 <jerryylx0325@gmail.com>
2024-07-25 13:52:34 -0400

    minor adjustment with navbar and styling

--- app.js
+++ app.js
@@ -1,42 +1,45 @@
 const express = require('express');
 const app = express();
-
 const fs = require('fs');
 const path = require('path');
-
 const PORT = 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-33
+// Get all posts and render them
 app.get('/', (req, res) => {
     const page = parseInt(req.query.page) || 1;
     const limit = 20;
+    // Calculate the start and end index of the posts to display
     const startIndex = (page - 1) * limit;
     const endIndex = page * limit;
 
+
+
     fs.readFile('./public/data/posts.json', 'utf8', (err, data) => {
-        try{
+        try {
             let posts = JSON.parse(data);
             const paginatedPosts = posts.slice(startIndex, endIndex);
-
-            res.render('index', { 
+            console.log(posts.length)
+            // Render the index.ejs template with the posts
+            res.render('index', {
+                numberOfPosts: posts.length,
                 posts: paginatedPosts,
                 currentPage: page,
                 totalPages: Math.ceil(posts.length / limit)
-             });
-        } catch{
+            });
+        } catch {
             res.status(500).send('Error parsing posts.json');
         }
-      
+
     });
 });
-
+//render all users profile
 app.get('/users', (req, res) => {
     fs.readFile('./public/data/users.json', 'utf8', (err, usersData) => {
-        try{
-          const users = JSON.parse(usersData);
+        try {
+            const users = JSON.parse(usersData);
             fs.readFile('./public/data/posts.json', 'utf8', (err, postsData) => {
                 if (err) {
                     res.status(500).send('Error reading posts data');
@@ -44,32 +47,41 @@ app.get('/users', (req, res) => {
                 }
                 const posts = JSON.parse(postsData);
                 res.render('users', { users, posts });
-          });
-        }catch{
+            });
+        } catch {
             res.status(500).send('Error parsing users.json');
             return;
         }
-        
+
     });
 });
-app.get ('/search' , (req,res)=>{
-  const query = req.query.search;
-  fs.readFile('./public/data/posts.json', 'utf8', (err, data) => {
-    try{
-        let posts = JSON.parse(data);
-        const searchResults = posts.filter(post => post.title.toLowerCase().includes(query.toLowerCase() 
-        || post.text.includes(query.toLowerCase())
-        || post.tags.includes(query.toLowerCase())));
-    
-        res.render('index', { 
-            posts: searchResults,
-            currentPage: 1,
-            totalPages: 1
-         });
-    } catch{
-        res.status(500).send('Error parsing posts.json');
-    }
-})})
+app.get('/search', (req, res) => {
+    const query = req.query.search;
+    const page = parseInt(req.query.page) || 1;
+    const limit = 20;
+
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    // Read the posts.json file and parse.
+    fs.readFile('./public/data/posts.json', 'utf8', (err, data) => {
+        try {
+            let posts = JSON.parse(data);
+            // Filter the posts based on the search query
+            const searchResults = posts.filter(post => post.title.toLowerCase().includes(query.toLowerCase()
+                || post.text.includes(query.toLowerCase())
+                || post.tags.includes(query.toLowerCase())));
+            const pagination = searchResults.slice(startIndex, endIndex);
+            res.render('index', {
+                numberOfPosts: searchResults.length,
+                posts: pagination,
+                currentPage: page,
+                totalPages: Math.ceil(searchResults.length / limit)
+            });
+        } catch {
+            res.status(500).send('Error parsing posts.json');
+        }
+    })
+})
 app.listen(PORT, () => {
     console.log('server started')
 });
